b1f8f711a7d7286f3a725319f29b66857b2f24db
Edwin Moreno <edwinmoreno77@gmail.com>
2022-05-31 18:41:25 -0400

    se agrego dataArr y se restauro crear html

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let calcular = document.querySelector('#calcular');
 let restaurar = document.querySelector('#restaurar');
 const divResultado = document.querySelector('#resultado');
 
+dataArr = [];
+
 calcular.addEventListener('click', () => {
 
   let capital = document.querySelector('#capital').value;
@@ -23,28 +25,57 @@ calcular.addEventListener('click', () => {
     capital = Math.round(capital);
     capital = capital / 100;
 
-    const crearHtml = () => {
-
-      const p = `<p class="p-3 m-2 aling-item-center"><b>${'mes/año '}${i}${' = '}${capital}</b></p>`;
-
-      const div = document.createElement('div');
-      div.classList.add('divHijo');
-      div.innerHTML = p;
-
-      divResultado.append(div)
-
-      return div
-    }
-
-    crearHtml();
+    const data = `mes/año ${i} = ${capital}`;
+    dataArr.push(data);
   }
 
+  crearHtml();
 })
+
+const crearHtml = () => {
+
+  const p = `<p class="p-3 m-2 aling-item-center"><b> ${dataArr[dataArr.length - 1]}</b></p>`;
+
+  const div = document.createElement('div');
+  div.classList.add('divHijo');
+  div.innerHTML = p;
+
+  return divResultado.append(div);
+
+}
+
+
 
 
 restaurar.addEventListener('click', () => {
 
   const divHijo = document.querySelector('.divHijo');
   divResultado.removeChild(divHijo);
+  dataArr = [];
 
 })
+
+
+
+
+
+
+
+
+
+//para imprimir todo el proceso mes a mes o año a año pegar esto dentro del For dentro de la funccion calcular y para evitar conflictos cambiar nombre de constantes y clases en conflicto.
+
+    // const crearHtml = () => {
+
+    //   const p = `<p class="p-3 m-2 aling-item-center"><b>${'mes/año '}${i}${' = '}${capital}</b></p>`;
+
+    //   const div = document.createElement('div');
+    //   div.classList.add('divHijo');
+    //   div.innerHTML = p;
+
+    //   divResultado.append(div)
+
+    //   return div
+    // }
+
+    // crearHtml();
